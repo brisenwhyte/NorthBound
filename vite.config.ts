b9be8c6da2267bd9dcbce687915256ec0c22e436
Whyte <brisen.whyte@siemens-healthineers.com>
2026-06-17 16:13:11 +0530

    hope

--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,11 @@ import { fileURLToPath } from "node:url";
 import tailwindcss from "@tailwindcss/vite";
 import { tanstackStart } from "@tanstack/react-start/plugin/vite";
 import viteReact from "@vitejs/plugin-react";
+import { nitro } from "nitro/vite";
 import { defineConfig } from "vite";
 import tsConfigPaths from "vite-tsconfig-paths";
 
-export default defineConfig({
+export default defineConfig(({ command }) => ({
   plugins: [
     tsConfigPaths(),
     tailwindcss(),
@@ -14,6 +15,10 @@ export default defineConfig({
       server: { entry: "server" },
     }),
     viteReact(),
+    command === "build" &&
+      nitro({
+        preset: "netlify",
+      }),
   ],
   resolve: {
     alias: {
@@ -21,4 +26,4 @@ export default defineConfig({
     },
     dedupe: ["react", "react-dom", "@tanstack/react-router", "@tanstack/react-start"],
   },
-});
+}));
